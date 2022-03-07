5775885daaf293d570a74c3d3500eb6fbd71d87c
Govind Kumar Bagla <govind.k.bagla@intimetec.com>
2022-03-07 11:47:21 +0530

    Add google anyalticls

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "./Context";
+import ReactGA from 'react-ga'
+ReactGA.initialize("UA-196802944-1")
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(
   <BrowserRouter >
   {/* <React.StrictMode> */}
